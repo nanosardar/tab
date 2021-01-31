f4918287d5ecd2de0c2fe4991e1c0c943c0a924f
nanosardar <53588795+nanosardar@users.noreply.github.com>
2021-01-31 22:35:17 +0300

    Add files via upload

---
+++ constants.dart
@@ -0,0 +1,4 @@
+const BASE_URL = "your url";
+const BASE_URL_IMAGE = "$BASE_URL/imageUpload/image";
+const NO_IMAGE_FOUND_URL =
+    "https://www.eveningtelegraph.co.uk/wp-content/uploads/sites/11/2017/04/McDonalds-e1493275164837.jpg";
